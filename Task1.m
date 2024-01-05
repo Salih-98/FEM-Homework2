dc356e5df5f9d4539481954531af692874d0d29c
User <salih.sahbegovic1998@gmail.com>
2024-01-05 12:30:28 +0100

    First task complete - new version

--- Task1.m
+++ Task1.m
@@ -51,7 +51,7 @@ x3 = 0; y3 = 0;
 A = a*(b+c)/2;
 
 %% Define B matrix
-Bu = (1/(2*A))*[y2-y3 y3-y1 0;0 0 x1-x3; x3-x2 x1-x3 y3-y1];
+Bu = (1/(2*A))*[y1-y2 y2-y3 0;0 0 x3-x2; x2-x1 x3-x2 y2-y3];
 
 %% Define stiffness tensor - plain strain state
 C = (E / ((1+v)*(1-2*v))) * [1-v v 0; v 1-v 0; 0 0 (1-2*v)/2];
@@ -61,7 +61,7 @@ Kuu = A * t * Bu' * C * Bu;
 
 
 %% Define Btheta 
-Btheta = [y2-y3 y3-y1 0;x3-x2 x1-x3 y3-y1];
+Btheta = [y1-y2 y2-y3 y3-y1;x2-x1 x3-x2 x1-x3];
 
 %% Define lambda matrix
 lambda = lambda * eye(2,2);
@@ -71,3 +71,31 @@ Ktheta = Btheta' * lambda * Btheta * A *t;
 
 %% Define reduced global coupling matrix
 Kutheta = (A*t)/3 * Bu' * C* alfa0 * eye(3,3);
+
+%% Define the consistent load vector
+alfa = atand(a/b);
+L = sqrt(a^2 + b^2);
+eqLoad2 = (pmax*L)/6;
+Ru = [0; eqLoad2*cosd(alfa); -eqLoad2*sind(alfa)];
+
+%% Static condensation
+K = [Kuu -Kutheta; zeros(3,3) Ktheta];
+rhs11 = Kutheta*thetaRef*ones(3,1);
+rhs12 = Ru - rhs11;
+Kn = getSortedMatrix(K);
+rhs = [rhs12; 0] - Kn(1:4,5:6)*[theta1; theta2];
+Knr = getReducedMatrix(Kn);
+u = inv(Knr) * rhs;
+
+
+function sortedMatrix = getSortedMatrix(K)
+sortedMatrix = K;
+sortedMatrix = [sortedMatrix(:, 1:3), sortedMatrix(:,6), sortedMatrix(:,4:5)];
+sortedMatrix = [sortedMatrix(1:3,:); sortedMatrix(6,:); sortedMatrix(4:5,:)];
+end
+
+function reducedMatrix = getReducedMatrix(K)
+reducedMatrix = K;
+reducedMatrix(:, 5:6) = [];
+reducedMatrix(5:6, :) = [];
+end
